03a71a06679e750b769634e01557e8006a639f8e
dhirajsah26 <ishantsah@gmail.com>
2024-12-09 15:56:30 -0500

    Intial commit

---
+++ src/main/java/np/com/dhirajsah/movies/MovieService.java
@@ -0,0 +1,33 @@
+package np.com.dhirajsah.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Console;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository repository;
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> findAllMovies() {
+        List<Movie> movies = repository.findAll();
+        movies.forEach(movie -> {
+            System.out.println("Generated toString: " + movie.toString());
+        });
+        return movies;
+    }
+
+    public Optional<Movie> findMovieById(ObjectId id) {
+        return movieRepository.findById(id);
+    }
+
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
